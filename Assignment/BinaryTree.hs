c28453ade697fa65fd5d85e328876065b4d6c32a
caglarih <caglarih@itu.edu.tr>
2016-04-28 23:28:00 +0300

    instances of Tree1 and 2 are added

--- Assignment/BinaryTree.hs
+++ Assignment/BinaryTree.hs
@@ -18,3 +18,29 @@ class BinaryTree t where
 
 data Tree1 a = Leaf1 a | Node1 a (Tree1 a) (Tree1 a)
 data Tree2 a = Leaf2 a | Node2 (Tree2 a) (Tree2 a)
+
+instance BinaryTree Tree1 where
+  leftTree t = case t of
+    Leaf1 _     -> Nothing
+    Node1 _ l _ -> Just l
+  rightTree t = case t of
+    Leaf1 _     -> Nothing
+    Node1 _ _ r -> Just r
+  rootValue t = case t of
+    Leaf1 n     -> Just n
+    Node1 n _ _ -> Just n
+  leaf = Leaf1
+  node = Node1
+
+instance BinaryTree Tree2 where
+  leftTree t = case t of
+    Leaf2 _   -> Nothing
+    Node2 l _ -> Just l
+  rightTree t = case t of
+    Leaf2 _   -> Nothing
+    Node2 _ r -> Just r
+  rootValue t = case t of
+    Leaf2 n   -> Just n
+    Node2 _ _ -> Nothing
+  leaf = Leaf2
+  node _ = Node2
